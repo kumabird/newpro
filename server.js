390adf334f98d86c5a15d65a88e2939100b43ff4
kumabird <kumakumakuumakuuma@gmail.com>
2026-02-20 21:01:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -172,10 +172,10 @@ const SIDEBAR_HTML = `
 <div id="sidebar" class="sidebar">
   <a href="/"><span class="sidebar-icon">🏠</span> <span class="sidebar-text">ホーム</span></a>
   <a href="/channel-search"><span class="sidebar-icon">📺</span> <span class="sidebar-text">チャンネル検索</span></a>
+  <a href="/music"><span class="sidebar-icon">♫</span> <span class="sidebar-text">Music</span></a>
   <a href="/history"><span class="sidebar-icon">🕘</span> <span class="sidebar-text">履歴</span></a>
   <a href="/admin"><span class="sidebar-icon">⚙️</span> <span class="sidebar-text">管理者ページ</span></a>
   <a href="/logout"><span class="sidebar-icon">🚪</span> <span class="sidebar-text">ログアウト</span></a>
-  <a href="/music"><span class="sidebar-icon">♫</span> <span class="sidebar-text">Music</span></a>
 </div>
 `;
 
