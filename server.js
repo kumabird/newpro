4c3a44378e2a76f0247b603d75671dae81f50be2
kumabird <kumakumakuumakuuma@gmail.com>
2026-03-26 14:24:37 +0900

    Update server.js

--- server.js
+++ server.js
@@ -164,7 +164,12 @@ const CSS = `
 const SIDEBAR_HTML = `
 <div id="sidebar" class="sidebar">
   <a href="/"><span class="sidebar-icon">🏠</span> <span class="sidebar-text">ホーム</span></a>
-  <a href="/channel-search"><span class="sidebar-icon">📺</span> <span class="sidebar-text">チャンネル検索</span></a>
+<form action="/channel-search" method="POST">
+  <button style="all:unset;cursor:pointer;">
+    <span class="sidebar-icon">📺</span>
+    <span class="sidebar-text">チャンネル検索</span>
+  </button>
+</form>
   <a href="/music"><span class="sidebar-icon">♫</span> <span class="sidebar-text">Music</span></a>
   <a href="/history"><span class="sidebar-icon">🕘</span> <span class="sidebar-text">履歴</span></a>
   <a href="/admin"><span class="sidebar-icon">⚙️</span> <span class="sidebar-text">管理者ページ</span></a>
