1c870714037a568e19ded4c4ad5b9245a5e97cee
kumabird <kumakumakuumakuuma@gmail.com>
2026-03-26 18:28:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.json());
 
 // PostgreSQL 接続
 import pkg from "pg";
